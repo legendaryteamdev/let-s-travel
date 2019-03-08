79799deedad5990a97893338193f6545689570c9
ReounKompheareak <reounkompheareak@gmail.com>
2019-03-08 15:41:54 +0700

    LT-10 BottomNavigation

--- app/src/main/java/com/example/lets_travel/FavoriteFragment.kt
+++ app/src/main/java/com/example/lets_travel/FavoriteFragment.kt
@@ -8,18 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-
-class HomeFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
 
--- app/src/main/java/com/example/lets_travel/MainActivity.kt
+++ app/src/main/java/com/example/lets_travel/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.lets_travel
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.action_home -> {
+                replaceFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.action_favorite -> {
+                replaceFragment(FavoriteFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.action_language -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.action_location -> {
+                replaceFragment(LocationFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
