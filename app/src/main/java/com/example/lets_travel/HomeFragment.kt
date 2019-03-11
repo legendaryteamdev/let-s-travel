29dad0b3edacf64c68335a1d0ef69d4d669bafbc
ReounKompheareak <reounkompheareak@gmail.com>
2019-03-11 10:37:03 +0700

    LT-10 province list UI

--- app/src/main/java/com/example/lets_travel/HomeFragment.kt
+++ app/src/main/java/com/example/lets_travel/HomeFragment.kt
@@ -1,8 +1,11 @@
 package com.example.lets_travel
 
 
+import adapter.HomeAdapter
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,12 +17,29 @@ private const val ARG_PARAM2 = "param2"
 
 class HomeFragment : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    var myDataset = arrayOf("BATTAMBANG", "PHNOM PENH")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+        viewManager = LinearLayoutManager(context)
+        viewAdapter = HomeAdapter(myDataset)
+        recyclerView = view.findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+
+        }
+
+        return view
     }
 
 
