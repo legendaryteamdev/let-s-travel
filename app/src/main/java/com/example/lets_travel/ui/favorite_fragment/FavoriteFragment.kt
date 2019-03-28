f047a279e3e456b3f9bbbe373ecccf9c86661725
ReounKompheareak <reounkompheareak@gmail.com>
2019-03-28 08:35:56 +0700

    change app color and icons

--- app/src/main/java/com/example/lets_travel/ui/favorite_fragment/FavoriteFragment.kt
+++ app/src/main/java/com/example/lets_travel/ui/favorite_fragment/FavoriteFragment.kt
@@ -3,6 +3,7 @@ package com.example.lets_travel.ui.favorite_fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,7 +17,15 @@ class FavoriteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_favorite, container, false)
+
+
+        //Set AppBar
+        (activity as AppCompatActivity).setSupportActionBar(view.findViewById(R.id.favorite_toolbar))
+        (activity as AppCompatActivity).supportActionBar?.setTitle(R.string.favorite)
+
+
+        return view
     }
 
 
