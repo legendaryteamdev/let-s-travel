0a65ac855d5df47b569fb31a12e3febfc901d0e6
ReounKompheareak <reounkompheareak@gmail.com>
2019-03-28 23:18:55 +0700

    correct all UI

--- app/src/main/java/adapter/ProvinceDetailAdapter.kt
+++ app/src/main/java/adapter/ProvinceDetailAdapter.kt
@@ -5,9 +5,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.example.lets_travel.ui.visiting_place_detail.VisitingPlaceDetail
 import com.example.lets_travel.R
+import com.example.lets_travel.ui.visiting_place_detail.VisitingPlaceDetail
 
 class ProvinceDetailAdapter(private val mDataset: Array<String>) :
     RecyclerView.Adapter<ProvinceDetailAdapter.ViewHolder>() {
@@ -23,18 +22,16 @@ class ProvinceDetailAdapter(private val mDataset: Array<String>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.mLocationName.text = mDataset[position]
+
     }
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val mLocationName = itemView.findViewById<TextView>(R.id.location_name_txt)
-        init {
-            itemView.setOnClickListener(View.OnClickListener {
-                val intent = Intent(itemView.context, VisitingPlaceDetail()::class.java)
-                intent.putExtra(PROVINCE_NAME , mLocationName.text)
-                itemView.context.startActivity(intent)
-            })
-        }
+//        init {
+//            itemView.setOnClickListener(View.OnClickListener {
+//                val intent = Intent(itemView.context, VisitingPlaceDetail()::class.java)
+//                itemView.context.startActivity(intent)
+//            })
+//        }
     }
 }
