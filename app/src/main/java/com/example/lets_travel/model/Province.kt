36f43a98c14bbf503b4ffc7c04f4cd65471f3c1c
ReounKompheareak <reounkompheareak@gmail.com>
2019-03-28 08:35:56 +0700

    change app color and icons

--- app/src/main/java/com/example/lets_travel/model/Province.kt
+++ app/src/main/java/com/example/lets_travel/model/Province.kt
@@ -1,3 +1,2 @@
 package com.example.lets_travel.model
 
-data class Province(var provinceName : String)
--- app/src/main/java/com/example/lets_travel/ui/MainActivity.kt
+++ app/src/main/java/com/example/lets_travel/ui/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.widget.Toolbar
 import com.example.lets_travel.R
 import com.example.lets_travel.ui.favorite_fragment.FavoriteFragment
 import com.example.lets_travel.ui.home_fragment.HomeFragment
 import com.example.lets_travel.ui.nearby_fragment.NearByFragment
+import com.example.lets_travel.ui.setting_fragment.SettingFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +27,7 @@ class MainActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.action_language -> {
-
+                replaceFragment(SettingFragment())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.action_location -> {
@@ -47,7 +50,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         replaceFragment(HomeFragment())
+
+        bottomNavigation
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu , menu)
+        return true
+    }
+
+
+
 }
