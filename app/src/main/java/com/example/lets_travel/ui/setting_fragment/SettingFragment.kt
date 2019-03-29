f8dc0d3e34d23d55cd9a13da8ab1de7a77da6d83
ReounKompheareak <reounkompheareak@gmail.com>
2019-03-29 10:56:43 +0700

    change color

--- app/src/main/java/com/example/lets_travel/ui/setting_fragment/SettingFragment.kt
+++ app/src/main/java/com/example/lets_travel/ui/setting_fragment/SettingFragment.kt
@@ -2,6 +2,7 @@ package com.example.lets_travel.ui.setting_fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,7 +17,13 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_setting, container, false)
+
+        //Set AppBar
+        (activity as AppCompatActivity).setSupportActionBar(view.findViewById(R.id.setting_toolbar))
+        (activity as AppCompatActivity).supportActionBar?.setTitle(R.string.setting)
+
+        return view
     }
 
 }
